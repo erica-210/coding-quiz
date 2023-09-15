727d67aac2c7225952fe1dc65d44946ba4fbdaaf
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-09-15 17:30:42 -0500

    added pseducode

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// opening screen and button to start quiz
+// when start is clicked timer is activated
+// as soon as timer is activated first question pops up
+    // can keep quesetions and answers in array
+    // keep answer options in styled box
+// after question is answered, next question pops up
+// when question is answered incorrectly, time is subtracted from timer
+    // note at bottom of screen that answer is wrong
+// when all question is answered or time runs out, GAME OVER
+// when game is over, score can be saved under initals
+     // have pop up box to input initials 
+    // can save scores in local storage
+// can access scores through highscore section
+    // has go back button to return to main quiz
+    // can clear highscores feature
